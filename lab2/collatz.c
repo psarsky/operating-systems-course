88257f0ac74673023f00feabd417c8b997b90adc
Jakub Psarski <psarski@student.agh.edu.pl>
2025-03-11 14:06:11 +0100

    lab2 start

---
+++ lab2/collatz.c
@@ -0,0 +1,20 @@
+#include "collatz.h"
+
+int collatz_conjecture(int input) {
+    if (input % 2 == 0) 
+        return input / 2;
+    return 3 * input + 1;
+}
+
+int test_collatz_conjecture(int input, int max_iter, int *steps) {
+    int count = 0;
+    while (input != 1 && count < max_iter) {
+        steps[count++] = input;
+        input = collatz_conjecture(input);
+    }
+    if (input == 1) {
+        steps[count++] = 1;
+        return count;
+    }
+    return -1;
+}
